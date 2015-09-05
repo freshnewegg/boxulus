daf8e1d9bec220a6dc7e9242c0a964edddfb56f1
cannon-edgar <edgarwang95@gmail.com>
2015-09-04 20:51:29 -0400

    Initial Commit

---
+++ boxulus/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  boxulus
+//
+//  Created by Edgar Wang on 2015-09-04.
+//  Copyright (c) 2015 Edgar Wang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
